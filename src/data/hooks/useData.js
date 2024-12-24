6986175b44c3dbaa1e318da011dbc61651446121
LeeLuiza <Laurabeck1315@gmail.com>
2024-12-24 14:35:10 +0500

    react

--- src/data/hooks/useData.js
+++ src/data/hooks/useData.js
@@ -14,12 +14,43 @@ export const useData = () => {
   };
 }
 
+export const useDeleteTodoItem  = () => {
+  const client = useQueryClient();
+
+  const { mutate } = useMutation({
+      mutationFn: ({ id }) => {
+          return LocalStorage.deleteTodoItemFromLocalStorage(id);
+      },
+      onSuccess: () => {
+          client.invalidateQueries(['todo']);
+      },
+  });
+
+  return {
+      mutate
+  }
+}
+
+export const useUpdateTodoItem = () => {
+  const client = useQueryClient();
+  const { mutate } = useMutation({
+      mutationFn: ({ id, checked, priority }) => {
+          return LocalStorage.updateTodoItemInLocalStorage(id, checked, priority);
+      },
+      onSuccess: () => {
+          client.invalidateQueries(['todo']);
+      },
+  });
+
+  return {mutate}
+}
+
 export const useSaveNewTodoItem = () => {
   const client = useQueryClient();
 
   const {mutate, isPending, isSuccess} = useMutation({
-    mutationFn: ({title}) => {
-      const newTodoItem = new TodoItem(new Date().getTime(), title, false);
+    mutationFn: ({title, priority}) => {
+      const newTodoItem = new TodoItem(new Date().getTime(), title, false, priority);
       return LocalStorage.saveTodoItemToLocalStorage(newTodoItem)
     },
     onSuccess: () => {
